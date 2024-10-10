29cc16f541824d2a5c5027c1bc65dda37b9a8880
Nitin Singh <nitinpsingh46@gmail.com>
2024-10-10 10:19:51 +0530

    build initial auth stage

---
+++ authapp/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import AppUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppUser
+        fields = ['id', 'username', 'email']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = AppUser
+        fields = ('username', 'password', 'email')
+
+    def create(self, validated_data):
+        user = AppUser.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ authapp/views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import AppUser
+from .serializers import UserSerializer, RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = AppUser.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=205)
+        except Exception:
+            return Response(status=400)
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = AppUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
