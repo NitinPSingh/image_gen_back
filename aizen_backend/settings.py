08dedb8e791a6a85d4c808dafca822a39e657bc5
Nitin Singh <nitinpsingh46@gmail.com>
2024-10-11 16:32:57 +0530

    update secret KEY

--- aizen_backend/settings.py
+++ aizen_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y218=i_6%a+=tgkeube_%3wz_k0%^#6%zif7)7r_n29(59-=v5'
+SECRET_KEY =str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
